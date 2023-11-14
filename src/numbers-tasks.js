69c5024384599a65facc0f6aa80ab16f3311c090
Rashit Mirzyalalov <rashiton@yandex.ru>
2023-11-14 23:38:28 +0300

    fix: string

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -373,8 +373,9 @@ function toExponential(number, fractionDigits) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  const i = number.toFixed(fractionDigits);
+  return String(i);
 }
 
 /**
